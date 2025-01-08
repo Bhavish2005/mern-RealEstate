11f20375a14405fa1a1bf1ef993f41a8f74b7ef5
Bhavish2005 <pushkarna.bhavish2005@gmail.com>
2025-01-08 22:25:09 +0530

    Updated Sign up Api and Complete Sign in Api and Added MIddlewares

--- Api/Controllers/auth.controller.js
+++ Api/Controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup= async(req,res)=>{
+import { errorHandler } from "../Utilities/error.js";
+import jwt from "jsonwebtoken";
+export const signup= async(req,res,next)=>{
     const{username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
     const newUser=new User({username,email,password:hashedPassword});
@@ -8,7 +10,24 @@ export const signup= async(req,res)=>{
     await newUser.save()
    res.status(201).json("user Created ");
    } catch(error){
-    res.status(500).json(error.message);
+     next(errorHandler(550,"User Not Created"));
    }
     
 };
+export const signin= async(req,res,next)=>{
+    const{email,password}=req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser){
+         return next(errorHandler(404,"User Not Found"));
+        }
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,"Invalid Credentials"));
+            
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        res.cookie("access_token",token,{httpOnly:true,expires:new Date(Date.now()+24*60*60*1000)}).status(200).json(validUser );
+    } catch (error) {
+        next(error);
+    }
+    
+}
